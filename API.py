b1db7f83fd05b898da6d9da5685542bb728f35ae
srinaath narasimhan <srinaath2002@gmail.com>
2022-10-02 10:21:57 +0530

    cleaned

--- API.py
+++ API.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import fitz
-import io
 from PIL import Image
-import numpy as np
 import pytesseract
 import yake
 text=''
@@ -11,7 +9,8 @@ import spacy
 import pytextrank
 import PyPDF2
 import urllib.request 
-# from io import StringIO
+from cleantext import clean
+
 
 
 pytesseract.pytesseract.tesseract_cmd = 'D:/Tesseract-OCR/tesseract.exe'
@@ -133,12 +132,37 @@ def main_with_url(url):
     resp['keywords'] = getKeyWords(text)
     resp['summary']= getsummary(text)
     resp['img_descp'] = getImgDescp('mast.pdf')
+
+    resp['img_descp']=clean(resp["img_descp"],
+        fix_unicode=True,               # fix various unicode errors
+        to_ascii=True,                  # transliterate to closest ASCII representation
+        lower=True,                     # lowercase text
+        no_line_breaks=False,           # fully strip line breaks as opposed to only normalizing them
+        no_urls=False,                  # replace all URLs with a special token
+        no_emails=False,                # replace all email addresses with a special token
+        no_phone_numbers=False,         # replace all phone numbers with a special token
+        no_numbers=False,               # replace all numbers with a special token
+        no_digits=False,                # replace all digits with a special token
+        no_currency_symbols=False,      # replace all currency symbols with a special token
+        no_punct=False,                 # remove punctuations
+        replace_with_punct="",          # instead of removing punctuations you may replace them
+        replace_with_url="<URL>",
+        replace_with_email="<EMAIL>",
+        replace_with_phone_number="<PHONE>",
+        replace_with_number="<NUMBER>",
+        replace_with_digit="0",
+        replace_with_currency_symbol="<CUR>",
+        lang="en"                       # set to 'de' for German special handling
+    )
     return resp
 
 
 @app.post('/nlpmodel/')
 async def create_item(item: Item):
-    return main_with_url(item.url)
+    try:
+        return main_with_url(item.url)
+    except:
+        return {'summary':'','keywords':'',"img_descp":'','message':'there '}
 
 @app.get('/')
 async def index():
