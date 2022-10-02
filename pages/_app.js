3539e83b565a51ea42b74a720d9547bb5a8bfcc1
vedant-11 <v.vedantsingh.11@gmail.com>
2022-10-02 10:06:54 +0530

    added footer

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+import Navbar from "../Components/navbar";
+import Footer from "../Components/footer";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
+
+export default MyApp;
